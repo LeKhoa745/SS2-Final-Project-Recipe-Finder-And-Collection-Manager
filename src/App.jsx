d1906d970a400d172056c35f81351b03cd893eaa
LeKhoa745 <minhkhoale435@gmail.com>
2026-03-25 13:20:02 +0700

    Merge remote and local project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import Wishlist from "./pages/Wishlist";
@@ -8,11 +9,16 @@ import Login from "./pages/Login";
 function App() {
   return (
     <Router>
+      <Navbar />
+
       <Routes>
-        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
         <Route path="/wishlist" element={<Wishlist />} />
+
+        {/* PUT LOGIN ROUTE HERE */}
+        <Route path="/login" element={<Login />} />
+
       </Routes>
     </Router>
   );
