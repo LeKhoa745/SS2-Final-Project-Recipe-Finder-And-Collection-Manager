5b1df0d33f2c56306ffa9f57b1dea3d83057823d
LeKhoa745 <minhkhoale435@gmail.com>
2026-04-01 23:39:49 +0700

    git commit test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,38 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import Wishlist from "./pages/Wishlist";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
-function App() {
+// Hide the global Navbar on auth pages (they have their own headers)
+function AppLayout() {
+  const location = useLocation();
+  const authPaths = ["/login", "/signup"];
+  const hideNav = authPaths.includes(location.pathname);
+
   return (
-    <Router>
-      <Navbar />
-
+    <>
+      {!hideNav && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
         <Route path="/wishlist" element={<Wishlist />} />
-
-        {/* PUT LOGIN ROUTE HERE */}
         <Route path="/login" element={<Login />} />
-
+        <Route path="/signup" element={<Signup />} />
       </Routes>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppLayout />
     </Router>
   );
 }
 
-export default App;
+export default App;
